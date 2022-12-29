b9975c2be29d70cde5701df0bf4c847aea707af5
s2422980 <y.budnyk@student.utwente.nl>
2022-12-29 15:36:09 +0100

    Vue.JS frontend was added

--- server/app.py
+++ server/app.py
@@ -13,9 +13,10 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'you-will-never-guess'
 api_key = 'mJpXOSabo4pvepg4JaPEOMhBWUxmS86hQIvueQQezx4'
+app.config.from_object(__name__)
 py_un = PyUnsplash(api_key=api_key)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
@@ -28,24 +29,24 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 # --------------- API -------------------
 
-@app.route('/', methods=['POST', 'GET'])  # url for my app
-def home():
-    if request.method == "POST":
-        word = request.form['word-search']
-        search = py_un.search(type_='photos', per_page=50, query=word)
-        photos = list()
-        print(type(search))
-        for entry in search.entries:
-            photos.append(entry.link_download)
-            print(entry.link_download)
-        print(len(photos))
-        return render_template("concept.html", concept=word, photos=photos)
-    return render_template("home.html")
-
-
-@app.route('/concept', methods=['POST', 'GET'])  # url for my app
-def concept(concept, photos):
-    pass
+#@app.route('/', methods=['POST', 'GET'])  # url for my app
+# def home():
+#     if request.method == "POST":
+#         word = request.form['word-search']
+#         search = py_un.search(type_='photos', per_page=50, query=word)
+#         photos = list()
+#         print(type(search))
+#         for entry in search.entries:
+#             photos.append(entry.link_download)
+#             print(entry.link_download)
+#         print(len(photos))
+#         return render_template("concept.html", concept=word, photos=photos)
+#     return render_template("home.html")
+#
+#
+# @app.route('/concept', methods=['POST', 'GET'])  # url for my app
+# def concept(concept, photos):
+#     pass
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
