30e4b0e76a6c850468b7b6f312746d3827e6f6fb
s2422980 <y.budnyk@student.utwente.nl>
2023-01-11 13:28:27 +0100

    The book app tutorial is finished, now the app can be remade into the initial idea

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Photos from '../components/Photos.vue';
+import Books from '../components/Books.vue';
 import Ping from '../components/Ping.vue';
 
 Vue.use(Router);
@@ -9,9 +11,19 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/',
+      name: 'Books',
+      component: Books,
+    },
+    {
       path: '/ping',
       name: 'Ping',
       component: Ping,
+    },
+    {
+      path: '/photos',
+      name: 'Photos',
+      component: Photos,
     },
   ],
 });
--- server/app.py
+++ server/app.py
@@ -1,90 +1,121 @@
-from flask import Flask, jsonify
+import uuid
+from flask import Flask, url_for, request, redirect, jsonify
+from flask import render_template
+from pyunsplash import PyUnsplash
 from flask_cors import CORS
 
 
 # configuration
 DEBUG = True
 
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
+# ------------ ATOMIC VARS ---------------
 
-# enable CORS
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'you-will-never-guess'
+api_key = 'mJpXOSabo4pvepg4JaPEOMhBWUxmS86hQIvueQQezx4'
+app.config.from_object(__name__)
+py_un = PyUnsplash(api_key=api_key)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify('Hello World!')
+# ------------RESOURCES------------
+BOOKS = [
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+# --------------- API -------------------
+
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
+    if request.method == 'DELETE':
+        remove_book(book_id)
+        response_object['message'] = 'Book removed!'
+    return jsonify(response_object)
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
+# @app.route('/', methods=['GET'])
+# def home():
+#     return jsonify('Hello World!')
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('Damn I did it')
 
+@app.route('/', methods=['POST', 'GET'])  # url for my app
+def home():
+    response_object = {'status': 'success'}
+    if request.method == "POST":
+        post_data = request.get_json()
+        word = post_data.get('word')
+        search = py_un.search(type_='photos', per_page=50, query=word)
+        photos = list()
+        print(type(search))
+        for entry in search.entries:
+            photos.append(entry.link_download)
+            print(entry.link_download)
+        print(len(photos))
+        response_object['photos'] = photos
+        return jsonify(response_object)
+    return jsonify(response_object)
 
-if __name__ == '__main__':
-    app.run()
 
-# from flask import Flask, url_for, request, redirect, jsonify
-# from flask import render_template
-# from pyunsplash import PyUnsplash
-# from flask_cors import CORS
-#
-#
-# from datetime import datetime
-#
-#
-#
-#
-# # ------------ ATOMIC VARS ---------------
-#
-#
-# app = Flask(__name__)
-# # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# # app.config['SECRET_KEY'] = 'you-will-never-guess'
-# api_key = 'mJpXOSabo4pvepg4JaPEOMhBWUxmS86hQIvueQQezx4'
-# app.config.from_object(__name__)
-# py_un = PyUnsplash(api_key=api_key)
-# CORS(app, resources={r'/*': {'origins': '*'}})
-#
-#
-#
-#
-#
-#
-#
-#
-# # --------------- API -------------------
-#
-# #@app.route('/', methods=['POST', 'GET'])  # url for my app
-# # def home():
-# #     if request.method == "POST":
-# #         word = request.form['word-search']
-# #         search = py_un.search(type_='photos', per_page=50, query=word)
-# #         photos = list()
-# #         print(type(search))
-# #         for entry in search.entries:
-# #             photos.append(entry.link_download)
-# #             print(entry.link_download)
-# #         print(len(photos))
-# #         return render_template("concept.html", concept=word, photos=photos)
-# #     return render_template("home.html")
-# #
-# #
-# # @app.route('/concept', methods=['POST', 'GET'])  # url for my app
-# # def concept(concept, photos):
-# #     pass
-#
-# @app.route('/ping', methods=['GET'])
-# def ping_pong():
-#     return jsonify('pong!')
-#
-#
-#
-#
-#
-#
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# @app.route('/concept', methods=['POST', 'GET'])  # url for my app
+# def concept(concept, photos):
+#     pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
