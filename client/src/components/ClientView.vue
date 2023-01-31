0c88c6a56815610c7a8232be000b5375b4d3810f
s2422980 <y.budnyk@student.utwente.nl>
2023-01-31 19:16:59 +0100

    delete button is added to the photo object now

--- client/src/components/ClientView.vue
+++ client/src/components/ClientView.vue
@@ -3,6 +3,7 @@
     <div class="container">
       <!-- bootswatch cdn -->
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/darkly/bootstrap.min.css" integrity="sha384-B4morbeopVCSpzeC1c4nyV0d0cqvlSAfyXVfrPJa25im5p+yEN/YmhlgQP/OyMZD" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
       <div class="row">
         <h1 class="text-center text-uppercase text-black">
           Describe your Brand
@@ -47,6 +48,12 @@
                        @click="increaseWeight(photo_object.id, indexPhoto, weight)"></div>
                   <div class="right-side"
                        @click="decreaseWeight(photo_object.id, indexPhoto, weight)"></div>
+                    <div class="delete-button">
+                      <button @click="deletePhoto(photo_object.id, indexPhoto)" type="button"
+                              class="btn btn-danger btn-sm float-right">
+                              <i class="fas fa-times"></i>
+                      </button>
+                    </div>
                     </div>
                 <!--<div v-if="activePhotoButtons === indexPhoto && isHovered===true"
                      class="btn-group" role="group">
@@ -430,6 +437,11 @@ export default {
 }
 
 .right-side {
+  right: 0;
+}
+.delete-button {
+  position: absolute;
+  top: 0;
   right: 0;
 }
 </style>
